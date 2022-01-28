246844ea3cb451dab9e05055629dd6f3885cc006
Nicky Obreykov <nicky.obreykov@gmail.com>
2022-01-28 16:31:08 +0100

    Update tests with new instances for each test

--- app/src/test/java/decide/LIC3Test.java
+++ app/src/test/java/decide/LIC3Test.java
@@ -8,55 +8,61 @@ import decide.lic.LIC3;
 
 public class LIC3Test {
 
-    LIC3 lic3 = new LIC3();
-    Parameters parameters = new Parameters();
-
     // Test cases for false
     @Test
     public void pointsArrayHasTwoElements() {
+        Parameters parameters = new Parameters();
         parameters.AREA1 = 10;
         Point[] points = new Point[2];
         points[0] = new Point(1, 1);
         points[1] = new Point(1, 2);
 
+        LIC3 lic3 = new LIC3();
         assertFalse(lic3.compute(points, parameters));
     }
 
     @Test
     public void colinearPoints() {
-        parameters.AREA1 = 10;
+        Parameters parameters = new Parameters();
+        parameters.AREA1 = 0;
         Point[] points = new Point[3];
         points[0] = new Point(1, 1);
         points[1] = new Point(1, 2);
         points[2] = new Point(1, 3);
 
+        LIC3 lic3 = new LIC3();
         assertFalse(lic3.compute(points, parameters));
     }
 
     @Test
     public void triangleLessThanArea() {
+        Parameters parameters = new Parameters();
         parameters.AREA1 = 10;
         Point[] points = new Point[3];
         points[0] = new Point(1, 1);
         points[1] = new Point(2, 2);
         points[2] = new Point(1, 5);
 
+        LIC3 lic3 = new LIC3();
         assertFalse(lic3.compute(points, parameters));
     }
 
     @Test
     public void triangleExactlyArea() {
+        Parameters parameters = new Parameters();
         parameters.AREA1 = 1;
         Point[] points = new Point[3];
         points[0] = new Point(1, 1);
         points[1] = new Point(2, 2);
         points[2] = new Point(1, 3);
 
+        LIC3 lic3 = new LIC3();
         assertFalse(lic3.compute(points, parameters));
     }
 
     @Test
     public void triangleNonConsecutiveGreaterThanArea() {
+        Parameters parameters = new Parameters();
         parameters.AREA1 = 3;
         Point[] points = new Point[4];
         points[0] = new Point(1, 1);
@@ -64,23 +70,27 @@ public class LIC3Test {
         points[2] = new Point(3, 3);
         points[3] = new Point(1, 5);
 
+        LIC3 lic3 = new LIC3();
         assertFalse(lic3.compute(points, parameters));
     }
 
     // Test cases for true
     @Test
     public void triangleGreaterThanArea() {
+        Parameters parameters = new Parameters();
         parameters.AREA1 = 3;
         Point[] points = new Point[3];
         points[0] = new Point(1, 1);
         points[1] = new Point(3, 3);
         points[2] = new Point(1, 5);
 
+        LIC3 lic3 = new LIC3();
         assertTrue(lic3.compute(points, parameters));
     }
 
     @Test
     public void triangleLastPointsGreaterThanArea() {
+        Parameters parameters = new Parameters();
         parameters.AREA1 = 3;
         Point[] points = new Point[4];
         points[0] = new Point(3, 3);
@@ -88,6 +98,7 @@ public class LIC3Test {
         points[2] = new Point(3, 3);
         points[3] = new Point(1, 5);
 
+        LIC3 lic3 = new LIC3();
         assertTrue(lic3.compute(points, parameters));
     }
 
