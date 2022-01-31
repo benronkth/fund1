f880808ce712d484913987563ae5afcc7546bc61
Benjamin Ronneling <benron@kth.se>
2022-01-31 14:44:51 +0100

    Adds the missing functions

--- app/src/main/java/decide/Functions.java
+++ app/src/main/java/decide/Functions.java
@@ -83,4 +83,157 @@ public class Functions {
     public double getAreaTriangle (Point p1, Point p2, Point p3) {
         return 0.5 * (p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y));
     }
+
+
+     /**********************************************************************************
+     * | Test cases for getCircumscribedCirclesRadius
+     ***********************************************************************************/
+    
+    @Test
+    public void theCircumscribedCirclesRadiusOfAZeroTriangle() {
+        Functions functions = new Functions();
+        Point point1 = new Point(1, 1);
+        Point point2 = new Point(1, 1);
+        Point point3 = new Point(1, 1);
+        assertEquals(0, functions.getCircumscribedCirclesRadius(point1,point2,point3), precisionDelta);
+    }
+    
+    @Test
+    public void theCircumscribedCirclesRadiusOfATriangle() {
+        Functions functions = new Functions();
+        Point point1 = new Point(0, 0);
+        Point point2 = new Point(10, 10);
+        Point point3 = new Point(8, 8);
+        assertEquals(5*Math.sqrt(2), functions.getCircumscribedCirclesRadius(point1,point2,point3), precisionDelta);
+    }
+
+    
+    @Test
+    public void theCircumscribedCirclesRadiusOfARightTriangle() {
+        Functions functions = new Functions();
+        Point point1 = new Point(0, 0);
+        Point point2 = new Point(0, 10);
+        Point point3 = new Point(8, 0);
+        assertEquals(Math.sqrt(41), functions.getCircumscribedCirclesRadius(point1,point2,point3), precisionDelta);
+    }
+
+    /**
+     * Calculates the radius of a smallest circle (circumscribed circle) that 
+     * is created using a triangle. 
+     * 
+     * @param points the corner points of the triangle
+     * @return the radius of a smallest circle (circumscribed circle)
+     */
+    public double getCircumscribedCirclesRadius(Point point1, Point point2, Point point3){
+        // Sides triangle
+ 
+        double side1 = getDistance(point1, point2);
+        double side2 = getDistance(point2, point3);
+        double side3 = getDistance(point1, point3);
+
+         // Area of the triangle with the Heron's formula 
+        double s = (side1 + side2 + side3)/2; 
+        double areaTriangle = Math.sqrt(s*(s-side1)*(s-side2)*(s-side3));
+ 
+         // Get radius of the circumscribed circle of the triangle 
+         if(areaTriangle==0){
+            double max = (Math.max(Math.max(side1, side2),side3));
+            return max/2;
+         }
+        double circumscribedCircleRadius = (side1 * side2 * side3)/(4 * areaTriangle); 
+        return circumscribedCircleRadius;
+    }
+
+
+    
+    
+    /**
+     * Finds the mid point of the longest side of a triangle. This 
+     * function is useful when we want to draw a circle on the longest
+     * side of a triangle.
+     * 
+     * @param point1 the first point
+     * @param point2 the second point
+     * @return the midpoint between point1 and point2
+     */
+
+    public Point getMidPointOfTheLongestSideInTriangle(Point point1, Point point2, Point point3){
+
+        double side1 = getDistance(point1, point2);
+        double side2 = getDistance(point2, point3);
+        double side3 = getDistance(point1, point3);
+        double longestSide = Math.max(Math.max(side1, side2), side3);
+
+        if(longestSide == side1){
+            return getMidPointOfTwoCoordinates(point1, point2);
+        }else if(longestSide == side2){
+            return getMidPointOfTwoCoordinates(point2, point3);
+        }else{
+            return getMidPointOfTwoCoordinates(point1, point3);
+        }
+
+    }
+
+      /**
+     * Finds the mid point of the longest side of a triangle. This 
+     * function is useful when we want to draw a circle on the longest
+     * side of a triangle.
+     * 
+     * @param point1 the first point
+     * @param point2 the second point
+     * @return the midpoint between point1 and point2
+     */
+
+    public Point getPointOusideOfTheLongestSideInTriangle(Point point1, Point point2, Point point3){
+
+        double side1 = getDistance(point1, point2);
+        double side2 = getDistance(point2, point3);
+        double side3 = getDistance(point1, point3);
+        double longestSide = Math.max(Math.max(side1, side2), side3);
+
+        if(longestSide == side1){ 
+            return point3;
+        }else if(longestSide == side2){
+            return point1;
+        }else{
+            return point2;
+        }
+
+    }
+
+    
+    /**
+     * Finds the mid point between 2 points in 2d space.
+     * 
+     * @param point1 the first point
+     * @param point2 the second point
+     * @return the midpoint between point1 and point2
+     */
+    public Point getMidPointOfTwoCoordinates(Point point1, Point point2){
+        
+        return new Point((point1.x + point2.x)/2, (point1.y + point2.y)/2);
+    }
+
+  
+    
+    /**
+     * Determines if a point is outside a circle.
+     * 
+     * @param circlesCenter the circles center point
+     * @param radius the circles radius
+     * @param point the point in question
+     * @return true if the point is outside the circle and false otherwise.
+     */
+    public boolean isPointOutsideCircle(Point circlesCenter, double radius, Point point){
+        
+        double distanceFromCirclesCenterToThePoint = getDistance(circlesCenter, point);
+        
+        return distanceFromCirclesCenterToThePoint > radius;
+    }
+
+
+
+
+
+
 }
--- app/src/main/java/decide/lic/LIC8.java
+++ app/src/main/java/decide/lic/LIC8.java
@@ -17,22 +17,20 @@ public class LIC8 extends AbstractLIC {
 
         int totalPointsThatShouldBeConsecutive = 3 + parameters.A_PTS + parameters.B_PTS;
 
-        System.out.println("Test with radius: "+parameters.RADIUS1);
 
         for (int i = 0; i <= points.length - totalPointsThatShouldBeConsecutive; i++) {
             int firstIndex = i;
             int secondIndex = firstIndex + parameters.A_PTS + 1;
             int thirdIndex = secondIndex + parameters.B_PTS + 1 ; 
-            System.out.println("indexes: "+ firstIndex + " - "+secondIndex+" - "+thirdIndex + " Radius: "+parameters.RADIUS1);
+            
             Point circlesCenter = functions.getMidPointOfTheLongestSideInTriangle(points[firstIndex], points[secondIndex], points[thirdIndex]);
             Point pointOutsideCircle = functions.getPointOusideOfTheLongestSideInTriangle(points[firstIndex], points[secondIndex], points[thirdIndex]);
             if(functions.isPointOutsideCircle(circlesCenter, parameters.RADIUS1, pointOutsideCircle)){
-                System.out.println("returned true");
+               
                 return true;
                 
             };
-        }
-        System.out.println("returned false");
+        } 
         
         return false;
     }
