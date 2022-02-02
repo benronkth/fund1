a04a31310b725f29b471d1933be750711b49d642
Nicky Obreykov <nicky.obreykov@gmail.com>
2022-02-02 17:55:35 +0100

    Two test cases

--- app/src/test/java/decide/DecideTest.java
+++ app/src/test/java/decide/DecideTest.java
@@ -13,7 +13,7 @@ import decide.datastructures.PreliminaryUnlockingVector;
 import decide.datastructures.LogicalConnectorMatrix.LCMValue;
 
 public class DecideTest {
-    
+
     //Empty test
     @Test public void falseEmptyCase() {
         Point[] points = new Point[0];
@@ -30,11 +30,11 @@ public class DecideTest {
         params.LENGTH1 = 10;
         params.RADIUS1 = 1;
         Point[] points = new Point[] {
-            new Point(1, 1),
-            new Point(1 + params.LENGTH1 + 1, 1 + params.LENGTH1 +1),
-            new Point(0,0),
-            new Point(1,1),
-            new Point(2,2),
+                new Point(1, 1),
+                new Point(1 + params.LENGTH1 + 1, 1 + params.LENGTH1 + 1),
+                new Point(0, 0),
+                new Point(1, 1),
+                new Point(2, 2),
         };
         LogicalConnectorMatrix lcm = new LogicalConnectorMatrix();
         PreliminaryUnlockingVector puv = new PreliminaryUnlockingVector();
@@ -44,5 +44,65 @@ public class DecideTest {
         puv.matrix[1] = true;
         boolean result = Decide.decide(points, params, lcm, puv);
         assertTrue("Simple test case with LIC0 and LIC1", result);
+    }
+
+    @Test public void negativeTestCaseWithLIC3and9and11() {
+
+        Parameters params = new Parameters();
+        params.AREA1 = 1;
+        params.EPSILON = Math.PI / 4;
+        params.C_PTS = 1;
+        params.D_PTS = 1;
+        params.G_PTS = 2;
+
+        // LIC 3 and 9 is true, 11 is false
+        Point[] points = new Point[] {
+                new Point(2, 0),
+                new Point(0, 2),
+                new Point(0, 0),
+                new Point(3, 0),
+                new Point(0, 2),
+        };
+
+        LogicalConnectorMatrix lcm = new LogicalConnectorMatrix();
+        PreliminaryUnlockingVector puv = PreliminaryUnlockingVector.allTrue();
+        lcm.matrix[3][9] = LCMValue.ANDD;
+        lcm.matrix[9][3] = LCMValue.ANDD;
+        lcm.matrix[3][11] = LCMValue.ORR;
+        lcm.matrix[11][3] = LCMValue.ORR;
+        lcm.matrix[9][11] = LCMValue.ANDD;
+        lcm.matrix[11][9] = LCMValue.ANDD;
+        boolean result = Decide.decide(points, params, lcm, puv);
+        assertFalse("Negative test case with LIC3, 9 and 11", result);
+    }
+
+    @Test public void positiveTestCaseWithLIC3and7and9() {
+        Parameters params = new Parameters();
+        params.AREA1 = 1;
+        params.K_PTS = 3;
+        params.LENGTH1 = 1;
+        params.EPSILON = Math.PI / 4;
+        params.C_PTS = 1;
+        params.D_PTS = 1;
+
+        // LIC 3, 7 and 9 is true
+        Point[] points = new Point[] {
+                new Point(2, 0),
+                new Point(0, 2),
+                new Point(0, 0),
+                new Point(3, 0),
+                new Point(0, 2),
+        };
+
+        LogicalConnectorMatrix lcm = new LogicalConnectorMatrix();
+        PreliminaryUnlockingVector puv = PreliminaryUnlockingVector.allTrue();
+        lcm.matrix[3][7] = LCMValue.ANDD;
+        lcm.matrix[7][3] = LCMValue.ANDD;
+        lcm.matrix[3][9] = LCMValue.ANDD;
+        lcm.matrix[9][3] = LCMValue.ANDD;
+        lcm.matrix[7][9] = LCMValue.ANDD;
+        lcm.matrix[9][7] = LCMValue.ANDD;
+        boolean result = Decide.decide(points, params, lcm, puv);
+        assertTrue("Positive test case with LIC3, 7 and 9", result);
     }
 }
