6c69307768b4522ce806076053fda040d3577842
Eyskha <chloe.porion@gmail.com>
2022-02-02 17:39:09 +0100

    Two test cases 1 positive and 1 negative tests cases with LICs 1 5 10 12 13 14 that matter

--- app/src/test/java/decide/DecideTest.java
+++ app/src/test/java/decide/DecideTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import decide.lic.AbstractLIC;
 import org.junit.Test;
 
 import decide.datastructures.LogicalConnectorMatrix;
@@ -44,5 +45,112 @@ public class DecideTest {
         puv.matrix[1] = true;
         boolean result = Decide.decide(points, params, lcm, puv);
         assertTrue("Simple test case with LIC0 and LIC1", result);
+    }
+
+    /**
+     * True test LICs 1, 5, 10, 12, 13, and 14
+     *
+     * LIC1: points 1 2 3
+     * LIC5: points 4 5
+     * LIC10: points 5 8 10
+     * LIC12: points 3 6 and 4 7
+     * LIC13: points 5 8 10 and 1 2 3
+     * LIC14: points 5 8 10 and 4 7 9
+     */
+    @Test public void trueAtLeast6LICsTrueFocusOn6LICs () {
+        Parameters params = new Parameters();
+        // Set parameters
+        params.RADIUS1 = 1;
+        params.E_PTS = 2;
+        params.F_PTS = 1;
+        params.AREA1 = 2;
+        params.K_PTS = 2;
+        params.LENGTH1 = 2;
+        params.LENGTH2 = 1;
+        params.A_PTS = 2;
+        params.B_PTS = 1;
+        params.RADIUS2 = 2;
+        params.AREA2 = 1;
+        Point[] points = new Point[] {
+                new Point(0,0),
+                new Point(1,1),
+                new Point(2,2),
+                new Point(1,0),
+                new Point(0,0),
+                new Point(0,0),
+                new Point(0.5,0),
+                new Point(3,0),
+                new Point(0,0),
+                new Point(0,4)
+        };
+        LogicalConnectorMatrix lcm = new LogicalConnectorMatrix();
+        PreliminaryUnlockingVector puv = new PreliminaryUnlockingVector();
+        // Set puv and lcm values:
+        // AND for all pairs of true LIC
+        // OR when one of LIC true
+        // NOTUSED for all others cases
+        int[] LICsUsed = new int[] {1, 5, 10, 12, 13, 14};
+        for (int lic1: LICsUsed) {
+            for (int lic2 = 0; lic2<15 ; lic2++) {
+                lcm.matrix[lic1][lic2] = LCMValue.ORR;
+                lcm.matrix[lic2][lic1] = LCMValue.ORR;
+            }
+            for (int lic2: LICsUsed) {
+                lcm.matrix[lic1][lic2] = LCMValue.ANDD;
+                lcm.matrix[lic2][lic1] = LCMValue.ANDD;
+            }
+            puv.matrix[lic1] = true;
+        }
+        boolean result = Decide.decide(points, params, lcm, puv);
+        assertTrue("Positive test case with 6 LICs: 1 5 10 12 13 and 14", result);
+    }
+
+    /** False test LICs 1, 5, 10, 12, 13, and 14, LIC14 false **/
+    @Test public void falseAtLeast6LICsTrueAnd1FalseFocusOn6LICs () {
+        Parameters params = new Parameters();
+        // Set parameters
+        params.RADIUS1 = 1;
+        params.E_PTS = 2;
+        params.F_PTS = 1;
+        params.AREA1 = 2;
+        params.K_PTS = 2;
+        params.LENGTH1 = 2;
+        params.LENGTH2 = 1;
+        params.A_PTS = 2;
+        params.B_PTS = 1;
+        params.RADIUS2 = 2;
+        params.AREA2 = 0;
+        Point[] points = new Point[] {
+                new Point(0,0),
+                new Point(1,1),
+                new Point(2,2),
+                new Point(1,0),
+                new Point(0,0),
+                new Point(0,0),
+                new Point(0.5,0),
+                new Point(3,0),
+                new Point(0,0),
+                new Point(0,4)
+        };
+        LogicalConnectorMatrix lcm = new LogicalConnectorMatrix();
+        PreliminaryUnlockingVector puv = new PreliminaryUnlockingVector();
+        // Set puv and lcm values:
+        // AND for all pairs of true LIC
+        // OR when one of LIC true
+        // NOTUSED for all others cases
+        int[] LICsUsed = new int[] {1, 5, 10, 12, 13, 14};
+        for (int lic1: LICsUsed) {
+            for (int lic2 = 0; lic2<15 ; lic2++) {
+                lcm.matrix[lic1][lic2] = LCMValue.ORR;
+                lcm.matrix[lic2][lic1] = LCMValue.ORR;
+            }
+            for (int lic2: LICsUsed) {
+                lcm.matrix[lic1][lic2] = LCMValue.ANDD;
+                lcm.matrix[lic2][lic1] = LCMValue.ANDD;
+            }
+            puv.matrix[lic1] = true;
+        }
+        boolean result = Decide.decide(points, params, lcm, puv);
+        assertFalse("Negative test case with 6 LICs: 1 5 10 12 13 and 14, LIC14 false", result);
     }
 }
